5ef915dac465a175e8c28a7742430c9fcb38c671
valeriyalexeeva15 <foxalishca@mail.ru>
2026-01-13 15:45:26 +0300

    Add data class OutpostResource and ResourceManager

---
+++ src/OutpostResource.kt
@@ -0,0 +1,18 @@
+data class OutpostResource(
+    val id: Int,
+    val name: String,
+    var amount: Int
+){
+    override fun toString(): String {
+        return "Ваш ресурс: $id | имя: $name | количество: $amount"
+    }
+}
+fun main() {
+    val gas= OutpostResource ( id=1, name = "Gas", amount=100)
+    val mineral= OutpostResource(id=2, name = "Minerals", amount = 250)
+    println("Ycneх! Вы добыли дополнительное количество минералов: ${mineral.amount + 50}")
+        val bonusMineral= mineral.copy(id = 3, name= "Minerals Bonus", amount = mineral.amount + 50)
+        println(gas.toString())
+                println(mineral.toString())
+                println(bonusMineral.toString())
+    }
